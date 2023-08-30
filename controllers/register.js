0685d596fc1c0d90f2f1935ad60da2a5e7900a13
unknown <valy_empire@yahoo.com>
2023-08-30 22:59:03 +0300

    Fixed the bug from backend

--- controllers/register.js
+++ controllers/register.js
@@ -1,17 +1,21 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  console.log(req.body);
   if (!email || !name || !password) {
     return res.status(400).json("incolrect form submision");
   }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
+    console.log(trx);
     trx
+
       .insert({
         hash: hash,
         email: email,
       })
       .into("login")
       .returning("email")
+
       .then(() => {
         return trx("users")
           .returning("*")
@@ -26,7 +30,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  }).catch((err) => res.status(400).json(err));
 };
 
 export default { handleRegister };
